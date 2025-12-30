cdbdfd9fd0970f18408fe7f0150c897a31e1b66c
BURAK CATAL <70900830+burakctl@users.noreply.github.com>
2025-12-30 13:46:49 +0300

    v0.3 Character Controller Added

---
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _orientationTransform;
+
+    [Header("Movement Settings")]
+    [SerializeField] private KeyCode _movementKey;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _maxSpeed; // Maksimum hız limiti eklendi
+
+    [Header("Jump Settings")]
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpCooldown;
+    [SerializeField] private bool _canJump = true;
+
+    [Header("Slide Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+
+    private float _startYScale;
+    private bool _isSliding = false;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float _playerHeight = 2f;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
+
+    private Rigidbody _playerRigidbody;
+    private float _horizontalInput, _verticalInput;
+    private Vector3 _movementDirection;
+
+    private void Awake()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
+        _startYScale = transform.localScale.y;
+    }
+    
+    private void Start()
+    {
+        _canJump = true;
+    }
+    
+    private void Update()
+    {
+        SetInputs(); 
+        SetPlayerDrag();
+        LimitPlayerSpeed(); // Hız limiti kontrolü eklendi
+    }
+    
+    private void FixedUpdate()
+    {
+        SetPlayerMovement();
+    }
+
+    private void SetInputs()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(_slideKey))
+        {
+            _isSliding = true;
+            Debug.Log("Player Sliding!");
+        }
+        else if(Input.GetKeyDown(_movementKey))
+        {
+            _isSliding = false;
+            Debug.Log("Player Moving!");
+        }
+
+        else if(Input.GetKey(_jumpKey) && _canJump && IsGrounded())
+        {
+            _canJump = false;
+            SetPlayerJumping();
+            Invoke(nameof(ResetJumping), _jumpCooldown);
+        }
+    }
+
+    private void SetPlayerMovement()
+    {
+        _movementDirection = _orientationTransform.forward * _verticalInput +
+                            _orientationTransform.right * _horizontalInput;
+        
+        if(_isSliding)
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+        }
+        else
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        }
+    }
+
+    private void SetPlayerDrag()
+    {
+        if(_isSliding)
+        {
+            _playerRigidbody.linearDamping = _slideDrag;
+        }
+        else
+        {
+            _playerRigidbody.linearDamping = _groundDrag;
+        }
+    }
+
+    private void LimitPlayerSpeed() // Hile için önemli limit koy!!!
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+        
+        // Maksimum hızı kontrol et
+        if(flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, _playerRigidbody.linearVelocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void SetPlayerJumping()
+    {    
+        _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+        _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJumping()
+    {
+        _canJump = true;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
+    }
+}
